f47f57488c8efd129adaf19ffc04411f93e1db62
amir rahimi <mary.hmyouni@gmail.com>
2022-04-19 08:35:47 +0430

    project logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
+
+ 
+  let [usdAUD,setUsdAUD] = useState(0);
+  
+
+  let [usdValueInput,SetUsdValueInput] = useState('');
+ 
+ 
+  let [usdAUD_Value,setUsdAUD_Value] = useState(0);
+  
+
+  const fetchPrice = () => {
+    fetch('https://open.er-api.com/v6/latest/USD')
+    .then(result=>{
+      return result.json();
+    })
+    .then(data=>{
+      let useAUD = parseFloat(data.rates.AUD);
+      setUsdAUD(usdAUD);
+      setUsdAUD_Value(parseInt(usdValueInput | 0) * useAUD);
+
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <div>
+ 
+        <div><strong>{usdAUD_Value}</strong> <small> AUD</small></div>
+      <input 
+        type="number"
+        value={usdValueInput} 
+        onChange={e=>{SetUsdValueInput(e.target.value)}} 
+        placeholder="Enter USD price..." 
+      />
+      <button onClick={fetchPrice}>Convert</button>
     </div>
+  </div>
   );
 }
 
