b0743348cd8efe369a9f9be227766808f4ef7616
amir rahimi <mary.hmyouni@gmail.com>
2022-04-19 08:46:23 +0430

    add 4 currency

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 function App() {
 
  
+  let [usdEUR,setUsdEUR] = useState(0);
+  let [usdJPY,setUsdJPY] = useState(0);
+  let [usdGBP,setUsdGBP] =  useState(0);
   let [usdAUD,setUsdAUD] = useState(0);
-  
 
   let [usdValueInput,SetUsdValueInput] = useState('');
  
  
+  let [usdEUR_Value,setUsdEUR_Value] = useState(0);
+  let [usdJPY_Value,setUsdJPY_Value] = useState(0);
+  let [usdGBP_Value,setUsdGBP_Value] =  useState(0);
   let [usdAUD_Value,setUsdAUD_Value] = useState(0);
-  
+
 
   const fetchPrice = () => {
     fetch('https://open.er-api.com/v6/latest/USD')
@@ -19,9 +24,21 @@ function App() {
       return result.json();
     })
     .then(data=>{
+      let useEUR = parseFloat(data.rates.EUR);
+      let useJPY = parseFloat(data.rates.JPY);
+      let useGBP = parseFloat(data.rates.GBP);
       let useAUD = parseFloat(data.rates.AUD);
+
+      setUsdEUR(usdEUR);
+      setUsdGBP(usdGBP);
+      setUsdJPY(usdJPY);
       setUsdAUD(usdAUD);
+      
+      setUsdEUR_Value(parseInt(usdValueInput | 0) * useEUR);
+      setUsdGBP_Value(parseInt(usdValueInput | 0) * useGBP);
+      setUsdJPY_Value(parseInt(usdValueInput | 0) * useJPY);
       setUsdAUD_Value(parseInt(usdValueInput | 0) * useAUD);
+
 
     })
     .catch(err=>{
@@ -34,11 +51,12 @@ function App() {
 
   return (
     <div className="App">
-    
-
     <div>
- 
-        <div><strong>{usdAUD_Value}</strong> <small> AUD</small></div>
+      <div><strong>{usdAUD_Value}</strong> <small> AUD</small></div>
+      <div><strong>{usdGBP_Value}</strong> <small> GBP</small></div>
+      <div><strong>{usdJPY_Value}</strong> <small> JPY</small></div>
+      <div><strong>{usdEUR_Value}</strong> <small> EUR</small></div>
+
       <input 
         type="number"
         value={usdValueInput} 
