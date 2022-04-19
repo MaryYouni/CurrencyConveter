81357843b68ebabc280a63e07ff0be5d70dbf7a9
amir rahimi <mary.hmyouni@gmail.com>
2022-04-19 09:04:17 +0430

    styles

--- src/App.js
+++ src/App.js
@@ -51,19 +51,19 @@ function App() {
 
   return (
     <div className="App">
-    <div>
-      <div><strong>{usdAUD_Value}</strong> <small> AUD</small></div>
-      <div><strong>{usdGBP_Value}</strong> <small> GBP</small></div>
-      <div><strong>{usdJPY_Value}</strong> <small> JPY</small></div>
-      <div><strong>{usdEUR_Value}</strong> <small> EUR</small></div>
+    <div className="container">
+      <div className="price-output"><strong>{usdAUD_Value}</strong> <small> AUD</small></div>
+      <div className="price-output"><strong>{usdGBP_Value}</strong> <small> GBP</small></div>
+      <div className="price-output"><strong>{usdJPY_Value}</strong> <small> JPY</small></div>
+      <div className="price-output"><strong>{usdEUR_Value}</strong> <small> EUR</small></div>
 
-      <input 
+      <input className="price-input"
         type="number"
         value={usdValueInput} 
         onChange={e=>{SetUsdValueInput(e.target.value)}} 
         placeholder="Enter USD price..." 
       />
-      <button onClick={fetchPrice}>Convert</button>
+      <button className="btn-convert" onClick={fetchPrice}>Convert</button>
     </div>
   </div>
   );
